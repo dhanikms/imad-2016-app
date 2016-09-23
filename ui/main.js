d9b97bf6195ab738893c8c85dab584b1911e497b
dhanikms <msdhanik11@gmail.com>
2016-09-23 21:49:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,17 +3,17 @@ var button = document.getElementById('counter');
 
 button.onclick = function() {
 
-//create a request object
-var request = new XMLHttpRequest();
+    //create a request object
+    var request = new XMLHttpRequest();
 
-//Capture the reponse and store it in a variable
-request.onreadystatechange = function() {
-  if(request.readystate===XMLHttpRequest.DONE) {
-  //Take some action
-  if(request.status===200) {
+   //Capture the reponse and store it in a variable
+   request.onreadystatechange = function() {
+    if(request.readystate===XMLHttpRequest.DONE) {
+      //Take some action
+      if(request.status===200) {
       var counter = request.reponseText;
       var span = document.getElementById('count');
-span.innerHTML = counter.toString();
+      span.innerHTML = counter.toString();
       
         }
      } 
@@ -21,6 +21,6 @@ span.innerHTML = counter.toString();
 };
 
 // make the request
-request.open('GET','http://http://dhanikms.imad.hasura-app.io/counter',true);
+request.open('GET','http://dhanikms.imad.hasura-app.io/counter',true);
 request.send(null);
 };
