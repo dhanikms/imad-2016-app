8a83c56ac3cb2fa8af7331f5b9d10df6ca670aa0
dhanikms <msdhanik11@gmail.com>
2016-09-23 21:53:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,19 +3,18 @@ var button = document.getElementById('counter');
 
 button.onclick = function() {
 
-    //create a request object
+    //Create a request object
     var request = new XMLHttpRequest();
 
    //Capture the reponse and store it in a variable
    request.onreadystatechange = function() {
-    if(request.readystate===XMLHttpRequest.DONE) {
+    if(request.readystate === XMLHttpRequest.DONE) {
       //Take some action
-      if(request.status===200) {
+      if(request.status === 200) {
       var counter = request.reponseText;
       var span = document.getElementById('count');
       span.innerHTML = counter.toString();
-      
-        }
+       }
      } 
      //Not done yet
 };
