38b80a735da76cfbbb55305be0f61efac2c47523
dhanikms <msdhanik11@gmail.com>
2016-09-25 20:04:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,9 +95,9 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) { // URL:/submit-name?name=xxxxxx
     // Get the name from the request
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
   // JSON: Javascript Object Notation
